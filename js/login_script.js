0d7b80e54e604e20b304dd1c5a99fb01b2336e52
Jess Zambrano <jessy.05.31@gmail.com>
2024-10-26 01:11:11 -0500

    Agregar login_styles.css y login_script.js

--- js/login_script.js
+++ js/login_script.js
@@ -2,18 +2,16 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('errorMessage');
 
     // Simulación de credenciales correctas
-    const validUsername = "a";
-    const validPassword = "a";
+    const validPassword = "viernes";
 
-    if (username === validUsername && password === validPassword) {
-        window.location.href = "dashboard.html"; // Redirigir a la página principal del sistema
+    if (password === validPassword) {
+        window.location.href = "index.html"; // Redirigir a la página principal del sistema
     } else {
-        errorMessage.textContent = "Usuario o contraseña incorrectos.";
+        errorMessage.textContent = "Respuesta incorrecta ☹️";
         errorMessage.style.display = "block";
     }
 });
